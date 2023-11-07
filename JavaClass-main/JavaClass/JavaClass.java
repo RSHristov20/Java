222c7513b1191bac27c193a4320a5c9188426559
Rayan Hristov <85338766+RSHristov20@users.noreply.github.com>
2023-11-07 10:48:41 +0200

    Add files via upload

---
+++ JavaClass-main/JavaClass/JavaClass.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is another line");
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1 2 3 4 5");
+
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1 2 3 4 5");
+        System.out.println("1 2 3 4 5");
+        System.out.println("1 2 3 4 5");
+
+    }
+}
